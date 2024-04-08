bb177e3b867ebea0a5b9b5ab5922138704fa2210
nrossendy <82850882+nrossendy@users.noreply.github.com>
2024-04-07 23:06:09 -0300

    Update hello.c

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,3 +1,10 @@
+/* HelloWorld
+ * Crear un un programa hello.c que envíe a stdout la línea Hello, World! o similar.
+ * Nicolás Nahuel Rossendy
+ * 20240407
+ */
+
+
 #include <stdio.h>
 int main() {
 
